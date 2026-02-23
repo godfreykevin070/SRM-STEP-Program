542df3dd13d26dbc23dc846e103b037c4a77b1ed
godfreykevin070 <godfreykevin070@gmail.com>
2026-02-23 10:06:15 +0530

    UC3: Check using reverse string

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Input Text: ");
-        String word = input.next();
+        String original = input.nextLine();   // Full line input
 
-        boolean isPalindrome = true;
+        String reversed = "";
 
-        for (int i = 0; i < word.length() / 2; i++) {
-            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Reverse string using for loop
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);  // String Concatenation
         }
 
-        if (isPalindrome) {
+        // Compare using equals()
+        if (original.equals(reversed)) {
+            System.out.println("Reversed Text : " + reversed);
             System.out.println("Is it a palindrome? : true");
         } else {
+            System.out.println("Reversed Text : " + reversed);
             System.out.println("Is it a palindrome? : false");
         }
 
