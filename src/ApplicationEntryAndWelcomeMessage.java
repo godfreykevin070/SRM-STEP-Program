746de5f69edfa92b7a0e7315d146e5cb0af7234d
godfreykevin070 <godfreykevin070@gmail.com>
2026-03-17 10:38:40 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/ApplicationEntryAndWelcomeMessage.java
@@ -0,0 +1,13 @@
+public class ApplicationEntryAndWelcomeMessage {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("====================================");
+
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using the system.");
+    }
+}
